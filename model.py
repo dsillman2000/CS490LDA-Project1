81cf96a182140140b286b8e3604f629f8c72c46d
David Sillman <dsillman2000@yahoo.com>
2021-09-18 09:41:15 -0400

    Semi-functional RMI model

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ class MaxAbsoluteError(keras.metrics.Metric):
 
 # max_gate = keras.layers.Lambda(one_hot_argmax)
 
-def new_stage_model(next_stage_size=1, n_layers=2, n_neurons=32, **kwargs):
+def new_stage_model(next_stage_size=1, n_layers=2, n_neurons=32, initializer='identity', **kwargs):
     """
     Generates a tf.keras.Model that is an MLP with ReLU activations that
     will be wrapped in a layer and incorporated into the RMI model. Model
@@ -30,13 +30,13 @@ def new_stage_model(next_stage_size=1, n_layers=2, n_neurons=32, **kwargs):
     inp = keras.Input(shape=(1,))
     x = inp
     for l in range(n_layers):
-        x = keras.layers.Dense(n_neurons, activation='relu',kernel_initializer='identity')(x)
-    outp = keras.layers.Dense(next_stage_size, activation='relu',kernel_initializer='identity')(x)
+        x = keras.layers.Dense(n_neurons, activation='relu',kernel_initializer=initializer)(x)
+    outp = keras.layers.Dense(next_stage_size, activation='relu',kernel_initializer=initializer)(x)
     stage_model = keras.Model(inp, outp)
     stage_model.compile(**kwargs)
     return stage_model
     
-def build_fit_rmi_model(dataset, stages=(1,2,3), n_layers=2, n_neurons=32, n_epochs=10, verbose='auto', **kwargs):
+def build_fit_rmi_model(dataset, stages=(1,3,4), n_layers=2, n_neurons=32, n_epochs=10, verbose='auto', initializer='identity', **kwargs):
     """
     Generates and fits the component stage models to their corresponding subsets of
     the data. See psuedocode in _Algorithm 1_ for more info.
@@ -49,24 +49,28 @@ def build_fit_rmi_model(dataset, stages=(1,2,3), n_layers=2, n_neurons=32, n_epo
     for i in range(M):
         for j in range(stages[i]):
             next_stage_size = 1 if i + 1 == len(stages) else stages[i + 1]
-            nn = new_stage_model(next_stage_size=1, n_layers=n_layers, n_neurons=n_neurons, **kwargs)
+            nn = new_stage_model(next_stage_size=1, n_layers=n_layers, n_neurons=n_neurons, initializer=initializer, **kwargs)
             if len(tmp_records[i][j]) > 0:
                 nn.fit(tmp_records[i][j], epochs=n_epochs, verbose=verbose)#0)
             if len(index) <= i:
                 index.append([])
             index[i].append(nn)
             if i < M - 1:
-                xsub = [[] for _ in range(next_stage_size)]
-                ysub = [[] for _ in range(next_stage_size)]
+#                 xsub = [[] for _ in range(next_stage_size)]
+#                 ysub = [[] for _ in range(next_stage_size)]
                 tmp_records.append([])
-                for r in tmp_records[i][j]:
-                    rx, ry = r
-                    pred = int(index[i][j].predict(rx)[0] / maxind * next_stage_size)
-                    pred = max(min(pred, next_stage_size-1), 0)
-                    xsub[pred].append(rx)
-                    ysub[pred].append(ry)
+                pred = index[i][j].predict(tmp_records[i][j]).reshape(-1) / maxind * next_stage_size
+                pred = pred.astype(int)
+#                 print(pred)
+#                 for r in tmp_records[i][j]:
+#                     rx, ry = r
+#                     pred = int(index[i][j].predict(rx)[0] / maxind * next_stage_size)
+#                     pred = max(min(pred, next_stage_size-1), 0)
+#                     xsub[pred].append(rx)
+#                     ysub[pred].append(ry)
                 for p in range(next_stage_size):
-                    subset = tf.data.Dataset.from_tensor_slices((xsub[p], ysub[p]))
+                    tmp = np.array(list(tmp_records[i][j].as_numpy_iterator())).reshape(-1,2,1)[pred==p]
+                    subset = tf.data.Dataset.from_tensor_slices((tmp[:,0], tmp[:,1]))
                     if verbose: print(f"[i={i},j={j},p={p}]len(subset)={len(subset)}")
                     tmp_records[-1].append(subset)
     # TODO: Handle error_threshold w/ B-Trees n shiet
